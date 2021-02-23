cc0edc0046c8850f34f3f9c784d289240e0ef249
rifat <mahfuzrifat10@gmail.com>
2021-02-23 22:08:27 +0600

    added html to password

---
+++ MID_LAB_TASK_PHP_06/password.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <body>
+	<form method="POST" action="">
+		<fieldset>
+			<legend> <b>CHANGE PASSWORD</b></legend>
+				<table>
+				<tr>
+					<td>Current Password: </td>
+					<td> <input type="password" name="pass"/>
+					<br/> </td>
+				</tr>
+
+				<tr>
+					<td> New Password: </td>
+					<td> <input type="password" name="newpass"/> <br/></td>
+				</tr>
+
+				<tr>
+					<td>Retype New Password: </td>
+					<td> <input type="password" name="nenewpass"/> <br/></td>
+				</tr>
+			</table>
+			<hr>
+
+			<input type="submit" name="Submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+  </body>
+</html>
