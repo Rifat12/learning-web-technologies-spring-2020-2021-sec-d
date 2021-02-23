724da7f8bc92c466a085a882c2aa883048633785
rifat <mahfuzrifat10@gmail.com>
2021-02-23 22:06:27 +0600

    Fixed login page

--- MID_LAB_TASK_PHP_06/login.php
+++ MID_LAB_TASK_PHP_06/login.php
@@ -1,5 +1,34 @@
 <?php
+if(isset($_POST['Submit']))
+{
+  if(strlen($_POST["name"])<2)
+  {
+    echo "User Name can not contain at least 2 characters";
+  }
+  elseif(strlen($_POST['Password'])<8)
+  {
+    echo "Password can not not be less than 8 characters\n";
+  }
 
+  $check = false;
+  for ($i=0; $i < strlen($_POST['Password']); $i++) {
+    if($_POST['Password'][$i] === '@' || $_POST['Password'][$i] === '#' || $_POST['Password'][$i] === '$' || $_POST['Password'][$i] === '%')
+    {
+      $check = true;
+      break;
+
+    }
+  }
+
+  if($check === false)
+  {
+    echo "Password must contain at least one of these characters => (@, #, $, %) \n";
+  }
+
+  else{
+    echo "Validation successful";
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,41 +38,26 @@
     <title></title>
   </head>
   <body>
-    <fieldset>
-    <legend>
-        LOGIN
-    </legend>
-    <form action='#' method='POST'>
-        <table>
-            <tr>
-                <td>
-                    User Name:
-                </td>
-                <td>
-                    <input type="text" name='username'>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Password:
-                </td>
-                <td>
-                    <input type="password" name='password'>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <input type='checkbox'>Remember Me
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <input type='submit' value='Submit'>
-                    <a href='#'>Forgot Password?</a>
-                </td>
-            </tr>
-        </table>
-    </form>
-</fieldset>
+    <form method="POST" action="">
+  		<fieldset>
+  			<legend> <b>Login</b></legend>
+  				<table>
+  				<tr>
+  					<td>UserName: </td>
+  					<td> <input type="text" name="name"/>
+  					<br/> </td>
+  				</tr>
+
+  				<tr>
+  					<td> Password: </td>
+  					<td> <input type="password" name="Password"/> <br/></td>
+  				</tr>
+  			</table>
+  			<hr>
+  			<input type="checkbox" name="checkbox"> Remember Me
+  			<br> <br>
+  			<input type="submit" name="Submit" value="Submit"> <a href=""> Forgot Password? </a>
+  		</fieldset>
+  	</form>
   </body>
 </html>
