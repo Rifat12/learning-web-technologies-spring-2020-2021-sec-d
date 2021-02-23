aa401d6b80890da591fa7e459bc2444fac094cf4
rifat <mahfuzrifat10@gmail.com>
2021-02-23 21:49:08 +0600

    Added HTML to login

---
+++ MID_LAB_TASK_PHP_06/login.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <fieldset>
+    <legend>
+        LOGIN
+    </legend>
+    <form action='#' method='POST'>
+        <table>
+            <tr>
+                <td>
+                    User Name:
+                </td>
+                <td>
+                    <input type="text" name='username'>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Password:
+                </td>
+                <td>
+                    <input type="password" name='password'>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type='checkbox'>Remember Me
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type='submit' value='Submit'>
+                    <a href='#'>Forgot Password?</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
+  </body>
+</html>
