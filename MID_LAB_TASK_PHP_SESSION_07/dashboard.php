208aed8f415f193b64599a703a34763357c01e83
rifat <mahfuzrifat10@gmail.com>
2021-02-25 22:46:09 +0600

    fixed a bug

--- MID_LAB_TASK_PHP_SESSION_07/dashboard.php
+++ MID_LAB_TASK_PHP_SESSION_07/dashboard.php
@@ -41,7 +41,7 @@
                     <li><a href='./profile.php'>View Profile</a></li>
                     <li><a href='./editprofile.php'>Edit Profile</a></li>
                     <li><a href='./changepic.php'>Change Profile Picture</a></li>
-                    <li><a href='./changepass'>Change Password</a></li>
+                    <li><a href='./changepass.php'>Change Password</a></li>
                     <li><a href='./logout.php'>Logout</a></li>
                 </ul>
             </td>
